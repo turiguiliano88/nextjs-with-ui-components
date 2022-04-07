239ea9c0ca99cf056014f9feb6db5fb9b6b1ff82
Minh Turi <nguyenvanminh.vnu@gmail.com>
2022-04-07 22:14:47 +0200

    add sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,26 @@
+export const Sidebar = ({ children, enabled }) => {
+  return (
+    <>
+      <div
+        className={`${
+          !enabled && "hidden"
+        } fixed top-0 left-0 right-0 w-full h-full bg-gray-800/80 flex p-3`}
+      ></div>
+      <div
+        className={`${
+          enabled ? "translate-x-0" : "translate-x-[600px]"
+        } fixed top-0 right-0 h-full transition bg-white z-10 p-md flex flex-col w-[600px]`}
+      >
+        {children}
+      </div>
+    </>
+  );
+};
+
+export const SidebarContent = ({ children }) => {
+  return <div className="">{children}</div>;
+};
+
+export const SidebarAction = ({ children }) => {
+  return <div>{children}</div>;
+};
--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,36 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Login from "../components/forms/Login";
-import SignUp from "../components/forms/SignUp";
 import { useState } from "react";
-import Select from "../components/Select";
-import Toggle from "../components/Toggle";
 import Button from "../components/Button";
-import {
-  Modal,
-  ModalAction,
-  ModalContent,
-  ModalTitle,
-} from "../components/Modal";
+import { Sidebar, SidebarAction, SidebarContent } from "../components/SideBar";
 
 export default function Home() {
-  const options = [1, 3, 4, 5, 7];
-  const [country, setCountry] = useState(1);
-  const [confirm, setConfirm] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
   // const [email, setEmail] = useState("");
   // const [password, setPassword] = useState("");
   return (
-    <div className="flex w-full">
-      <div className="w-6/12 p-3">
-        <Login />
-        {/* <Toggle enabled={confirm} onChange={() => setConfirm(!confirm)} /> */}
-        {/* <Button onClick={() => setShowModal(!showModal)}>show Modal</Button> */}
+    <div className="">
+      <div className="flex justify-center">
+        <div className="w-6/12 p-3">
+          <Login />
+          {/* <Toggle enabled={confirm} onChange={() => setConfirm(!confirm)} /> */}
+          {/* <Button onClick={() => setShowModal(!showModal)}>show Modal</Button> */}
+        </div>
       </div>
-      <div className="w-6/12 p-3">
-        <SignUp />
+      {/* <div className="self-start">
+        <Button onClick={() => setShowSidebar(!showSidebar)}>
+          Show sidebar
+        </Button>
       </div>
+      <Sidebar enabled={showSidebar}>
+        <SidebarContent>
+          <p>This is sidebar content</p>
+        </SidebarContent>
+        <SidebarAction>
+          <Button onClick={() => setShowSidebar(!showSidebar)}>Ok</Button>
+        </SidebarAction>
+      </Sidebar> */}
     </div>
   );
 }
